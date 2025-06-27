29b5a92abb4e97395ddb523c064aa3d2486d6ff1
AndrewLHellman <142917243+AndrewLHellman@users.noreply.github.com>
2025-06-27 16:10:09 -0500

    Run ai on seperate thread

--- script.js
+++ script.js
@@ -1,95 +1,8 @@
 var board,
     game = new Chess();
 
-var minimaxRoot = function(depth, game, isMaximisingPlayer) {
-    var newGameMoves = game.ugly_moves();
-    var bestMove = -Infinity;
-    var bestMovesFound = [];
-
-    for (let i = 0; i < newGameMoves.length; i++) {
-        var newGameMove = newGameMoves[i];
-        game.ugly_move(newGameMove);
-        var value = minimax(depth - 1, game, -Infinity, Infinity, !isMaximisingPlayer);
-        game.undo();
-        if (value > bestMove) {
-            bestMove = value;
-            bestMovesFound = [newGameMove];
-        } else if (value === bestMove) {
-            bestMovesFound.push(newGameMove);
-        }
-    }
-    return bestMovesFound[Math.floor(Math.random() * bestMovesFound.length)];
-}
-
-var minimax = function (depth, game, alpha, beta, isMaximisingPlayer) {
-    positionCount++;
-    if (depth === 0) {
-        return -evaluateBoard(game.board());
-    }
-
-    var newGameMoves = game.ugly_moves();
-
-    if (isMaximisingPlayer) {
-        var bestMove = -Infinity;
-        for (let i = 0; i < newGameMoves.length; i++) {
-            game.ugly_move(newGameMoves[i])
-            bestMove = Math.max(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
-            game.undo();
-            alpha = Math.max(alpha, bestMove);
-            if (beta <= alpha) {
-                return bestMove;
-            }
-        }
-        return bestMove;
-    } else {
-        var bestMove = Infinity;
-        for (let i = 0; i < newGameMoves.length; i++) {
-            game.ugly_move(newGameMoves[i]);
-            bestMove = Math.min(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
-            game.undo();
-            beta = Math.min(beta, bestMove);
-            if (beta <= alpha) {
-                return bestMove;
-            }
-        }
-        return bestMove;
-    }
-}
-
-var evaluateBoard = function (board) {
-    var totalEvalutation = 0;
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            totalEvalutation += getPieceValue(board[i][j]);
-        }
-    }
-    return totalEvalutation;
-}
-
-var getPieceValue = function (piece) {
-    if (piece === null) {
-        return 0;
-    }
-    var getAbsoluteValue = function (piece) {
-        if (piece.type === 'p') {
-            return 10;
-        } else if (piece.type === 'r') {
-            return 50;
-        } else if (piece.type === 'n') {
-            return 30;
-        } else if (piece.type === 'b') {
-            return 30 ;
-        } else if (piece.type === 'q') {
-            return 90;
-        } else if (piece.type === 'k') {
-            return 900;
-        }
-        throw "Unknown piece type: " + piece.type;
-    };
-
-    var absoluteValue = getAbsoluteValue(piece, piece.color === 'w');
-    return piece.color === 'w' ? absoluteValue : -absoluteValue;
-};
+var worker = new Worker('chess-worker.js');
+var isCalculating = false;
 
 var onDragStart = function (source, piece, position, orientation) {
     if (game.in_checkmate() === true || game.in_draw() === true ||
@@ -99,35 +12,45 @@ var onDragStart = function (source, piece, position, orientation) {
 };
 
 var makeBestMove = function () {
-    var bestMove = getBestMove(game);
-    game.ugly_move(bestMove);
-    board.position(game.fen());
-    renderMoveHistory(game.history());
-    if (game.game_over()) {
-        reportGameEnd();
-    }
+    getBestMove(game)
 };
 
-var positionCount;
 var getBestMove = function (game) {
     if (game.game_over()) {
         reportGameEnd();
+        return;
     }
 
-    positionCount = 0;
+    if (isCalculating) return;
+
+    isCalculating = true;
+    $('#position-count').text('Calculating...');
+
     var depth = parseInt($('#search-depth').find(':selected').text());
 
-    var d = new Date().getTime();
-    var bestMove = minimaxRoot(depth, game, true);
-    var d2 = new Date().getTime();
-    var moveTime = (d2 - d);
-    var positionsPerS = ( positionCount * 1000 / moveTime);
+    worker.postMessage({
+        fen: game.fen(),
+        depth: depth
+    });
+};
+
+worker.onmessage = function(e) {
+    var { bestMove, positionCount, moveTime, positionsPerS } = e.data;
 
     $('#position-count').text(positionCount);
     $('#time').text(moveTime/1000 + 's');
     $('#positions-per-s').text(positionsPerS);
-    return bestMove;
-};
+
+    game.ugly_move(bestMove);
+    board.position(game.fen());
+    renderMoveHistory(game.history());
+
+    isCalculating = false;
+
+    if (game.game_over()) {
+        reportGameEnd();
+    }
+}
 
 var reportGameEnd = function() {
     var result = '';
